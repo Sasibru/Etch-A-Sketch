cacd60f277aa8d71ea0828d976de6d0c9aae3ecc
Sander Brustad <sander.simonsen.brustad@gmail.com>
2022-12-09 15:07:45 +0100

    Add grid

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
 "use strict"
+
+const gridContainer = document.getElementById("gridContainer");
+
+function createRows(rows, columns) {
+    gridContainer.style.setProperty("--grid-rows", rows);
+    gridContainer.style.setProperty("--grid-columns", columns);
+    for(let i = 0; i < (rows * columns); i++) {
+        let cell = document.createElement("div");
+        cell.innerText = (i + 1);
+        gridContainer.appendChild(cell).className = "grid-item";
+    };
+};
+
+createRows(16,16)
