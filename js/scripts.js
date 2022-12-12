372302d8980a9adfda1bc356d26ef0676b1ab29c
Sander Brustad <sander.simonsen.brustad@gmail.com>
2022-12-12 12:51:46 +0100

    Add WIP of resizing grid

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,6 @@ function createRows(rows, columns) {
     gridContainer.style.setProperty("--grid-columns", columns);
     for(let i = 0; i < (rows * columns); i++) {
         let cell = document.createElement("div");
-        cell.innerText = (i + 1);
         gridContainer.appendChild(cell).className = "grid-item";
     };
 };
